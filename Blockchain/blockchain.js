ff16005df6994bb32c91976a9eafcad0ca553e09
gaurav1209 <aggarwal.gaurav10@gmail.com>
2019-03-16 20:46:19 +0530

    Added Hashing and Proof Of Work

--- Blockchain/blockchain.js
+++ Blockchain/blockchain.js
@@ -1,3 +1,6 @@
+const sha = require('sha256');
+
+
 // Blockchain Constructor Function
 // Chains - Chain Data
 // Pending Transactions - Transactions made since last Block is pushed into the chain
@@ -5,6 +8,9 @@
 function Blockchain() {
     this.chains = [];
     this.pendingTransactions = [];
+
+    // Genesios Block
+    this.createNewBlock(1, '0', '0');    
 }
 
 // Creating New Blocks to be pushed into the chain
@@ -40,6 +46,26 @@ Blockchain.prototype.newTransaction = function(sender, receiver, amount) {
     this.pendingTransactions.push(transaction);
 
     return this.getLastBlock()['index'] + 1;
+}
+
+// Hash Block
+Blockchain.prototype.hashBlock = function(nonce, prevHash, currentBlock) {
+    const dataStr = prevHash + nonce.toString() + JSON.stringify(currentBlock);
+    const hash = sha(dataStr);
+    return hash;
+}
+
+// Proof Of Work
+Blockchain.prototype.proofOfWork = function(prevHash, currentBlock) {
+    let nonce = 0;
+    let hash = this.hashBlock(nonce, prevHash, currentBlock);
+
+    while(hash.substr(0,4) != '0000'){
+        nonce++;
+        hash = this.hashBlock(nonce, prevHash, currentBlock);
+    }
+
+    return nonce;
 }
 
 module.exports = Blockchain ;
