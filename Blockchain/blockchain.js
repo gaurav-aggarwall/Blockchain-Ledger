4e486b19d4b174182dd716e8d9181c3c6dec8ee2
gaurav1209 <aggarwal.gaurav10@gmail.com>
2019-03-16 16:55:41 +0530

    Initial Commit

---
+++ Blockchain/blockchain.js
@@ -0,0 +1,9 @@
+// Blockchain Constructor Function
+// Chains - Chain Data
+// Pending Transactions - Transactions made since last Block is pushed into the chain
+
+const Blockchain = () => {
+    this.chains = [];
+    this.pendingTransactions = [];
+}
+
